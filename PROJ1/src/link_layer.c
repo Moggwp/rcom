5911ef4769ccc00f932b519f47163c29aa0ba5fe
Telluwu <tellomewhy@gmail.com>
2025-03-19 00:43:21 +0000

    DLLstart

--- PROJ1/src/link_layer.c
+++ PROJ1/src/link_layer.c
@@ -1,16 +1,48 @@
 // Link layer protocol implementation
 
-#include "link_layer.h"
+#include "link_layer.h" // connectionParameters
 
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
 
+//var definition
+int timeout = 0;
+int retrans = 0;
+int alarmEnabled = FALSE;
+/*timeout = connectionParameters.timeout;
+    retrans = connectionParameters.nRetransmissions;*/
 ////////////////////////////////////////////////
 // LLOPEN
 ////////////////////////////////////////////////
-int llopen(LinkLayer connectionParameters)
+int llopen(LinkLayer connectionParameters) // param. given by app layer
 {
-    // TODO
+    State ActualState = START;
+
+    int fd = connection(connectionParameters.serialPort); //file descriptor of serialPort
+    if (fd < 0){
+        return -1; //error opening serial port
+    }
+
+    unsigned char byte;
+
+    switch (connectionParameters.role){
+        case LlTx: { //send SET to Rx
+
+            (void) signal(SIGALRM, alarmHandler);
+
+            while (STOP != TRUE && connectionParameters.nRetransmissions != 0){
+                sendSFrame(fd, A_TR, C_SET);
+                alarm(connectionParameters.timeout);
+                alarmEnabled = FALSE;
+
+                while (ActualState != END && alarmEnabled == FALSE) {
+
+                }
+           }
+
+        }
+    }
+        
 
     return 1;
 }
